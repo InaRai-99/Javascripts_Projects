24028c0cd0162c157d34673117460a111c49dcc0
Ina Rai <inarai148@gmail.com>
2026-02-19 18:56:53 +0000

    19/02

---
+++ Interactive Gallery/Script.js
@@ -0,0 +1,11 @@
+const thumbnail_container = document.getElementById("thumbnail-container")
+const main_image = document.getElementById("main-image")
+function updateMainImage(event) 
+{ 
+    console.log(event.target.tagName)
+    if(event.target.tagName === "IMG") 
+    {
+        main_image.src = event.target.src
+    }
+}
+thumbnail_container.addEventListener("click", updateMainImage)
